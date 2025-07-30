464835ccec8a269e6f77f4b13eed25f085e46690
J-odion <emmyjaff22@gmail.com>
2025-07-30 12:55:29 +0100

    email auth

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -5,11 +5,14 @@ dotenv.config();
 
 export const transporter = nodemailer.createTransport({
 
-    host: 'kairosenergy.ng',
+    host: 'mail.kairosenergy.ng',
     port: 465,
     secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    authMethod: 'LOGIN',
+    logger: true,
+    debug: true, 
 });
