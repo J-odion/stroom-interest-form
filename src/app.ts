8d2f269cd15dae2a778ca9abe9ba04530f36f875
J-odion <emmyjaff22@gmail.com>
2025-08-01 21:32:07 +0100

    adding port

--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,9 @@ const app = express();
 const ALLOWED_ORIGINS = [
   "https://credit.kairoshof.com",
   "http://localhost:3000",
-  "https://credit-database.vercel.app"
+  "https://credit-database.vercel.app",
   // "http://localhost:3002",
-  // "http://localhost:3001/",
+  "http://localhost:3001/"
 ];
 
 // ✅ Type-safe CORS configuration
